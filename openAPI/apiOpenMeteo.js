4f2fb222c280d531f075fac988b464389001d349
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-06-09 00:20:42 -0500

    psuedo code for js

--- openAPI/apiOpenMeteo.js
+++ openAPI/apiOpenMeteo.js
@@ -1,1 +1,35 @@
-/// JS for open-meteo
+// setting variables
+// weather-form
+// city-input
+// location
+// temperature
+
+// function geocoding user's city for get long and lat
+    // set url
+    
+// function getWeather
+    // set url
+
+    // try
+        // response
+        // data
+
+        // plug in temperature
+    // error
+        // display error message
+        // console.error
+
+// add event listener to form
+    // preventDefault
+    // get city name
+
+    // try 
+        // get location
+        // get long and lat
+
+        // update location name
+        // update temperature
+
+    // error
+        // display error message
+        // console.error
